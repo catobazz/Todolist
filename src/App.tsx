e9ef92247c761b53cebfe65793d27ffbca2e5c36
Cato13 <catobazzz@gmail.com>
2023-04-24 21:39:47 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import TodoList, {TaskType} from "./TodoList";
+
+
+
+function App() {
+    const tasks: Array<TaskType> = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+    ]
+
+    return (
+
+        <div className="App">
+           <TodoList
+               tasks={tasks}
+               title ="What to learn" />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Count.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type CountPropsType = {
+    count: number;
+}
+
+const Count = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Count;
