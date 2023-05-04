6e8cfe347dfc58f620cb876d8dfd4c99aba5bd15
ArtemR <catobazzz@gmail.com>
2023-05-04 16:05:10 +0300

    add new branch todolist_dimy4

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import {TaskType, TodoList} from "./components/TodoList";
+
+
+function App() {
+    let task1: Array<TaskType> = [ /*прописали тип массива, импортировали тип тасок из TaskType*/
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false}
+    ]
+    let task2: Array<TaskType> = [
+        {id: 1, title: "Terminafter", isDone: true},
+        {id: 2, title: "BBC", isDone: true},
+        {id: 3, title: "XXX", isDone: false}
+    ]
+
+    return (
+        <div className="App">
+
+            <TodoList title="What to learn" tasks={task1}/>
+            <TodoList title="Moovies" tasks={task2}/>
+            {/*отрисовывает JSX из компоненты todolist (передает в todolist паремтры title и tasks через props)*/}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export type TaskType = { /*тип тасок в PropsType*/ /*экспортируем тип тасок в task1 и task2 (в App)*/
+    id: number,
+    title: string,
+    isDone: boolean
+}
+type PropsType = {   /*тип входящих props в todolist*/
+    title: string,
+    tasks: Array<TaskType> /*сюда передается тип тасок из TaskType*/
+}
+export const TodoList = (props: PropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
