c36547a1a38a1bfcf480ffc0b4fce53ea9fff2ee
ArtemR <catobazzz@gmail.com>
2023-05-05 13:13:34 +0300

    upd useState (del let initeTasks)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TaskType, TodoList} from "./components/TodoList";
+import {TodoList} from "./components/TodoList";
 
 
 function App() {
-    let initTasks: Array<TaskType> = [ /*прописал тип массива, импортировал тип тасок из TaskType*/
+    /*хук useState для перерисовки массива initTasks*/
+    let [tasks, setTasks] = useState([
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
         {id: 4, title: "JAVA", isDone: false}
-    ]
-    /*хук useState для перерисовки массива initTasks*/
-    let [tasks, setTasks] = useState(initTasks);
+    ]);
 
-    /*removeTasks принимает id из массива initTasks*/
+    /*removeTasks принимает id из массива*/
     function removeTasks(id: number) {
         let filteredTasks = tasks.filter(t => t.id !== id) /*пропускает все id которые !== той которую надо удалить. Передает новый массив в setTasks для отправки в стейт на перерисовку*/
         setTasks(filteredTasks);
