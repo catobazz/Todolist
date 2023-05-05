5f7ede256e7b711d63c4c1dffbc79be584cb64d8
ArtemR <catobazzz@gmail.com>
2023-05-05 10:16:21 +0300

    add func removeTasks

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,25 @@ import {TaskType, TodoList} from "./components/TodoList";
 
 
 function App() {
-    let task1: Array<TaskType> = [ /*прописал тип массива, импортировал тип тасок из TaskType*/
+    let tasks: Array<TaskType> = [ /*прописал тип массива, импортировал тип тасок из TaskType*/
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
         {id: 4, title: "JAVA", isDone: false}
     ]
-    let task2: Array<TaskType> = [
-        {id: 1, title: "Terminafter", isDone: true},
-        {id: 2, title: "BBC", isDone: true},
-        {id: 3, title: "XXX", isDone: false}
-    ]
+
+    function removeTasks(id: number) {
+        tasks = tasks.filter(t => t.id !== id)
+    }
+
 
     return (
         <div className="App">
 
-            <TodoList title="What to learn" tasks={task1}/>
-            <TodoList title="Moovies" tasks={task2}/>
+            <TodoList title="What to learn"
+                      tasks={tasks}
+                      removeTasks={removeTasks}
+            />
             {/*отрисовывает JSX из компоненты todolist (передает в todolist паремтры title и tasks через props)*/}
         </div>
     );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,7 +7,8 @@ export type TaskType = { /*тип тасок в PropsType*/ /*экспортир
 }
 type PropsType = {   /*тип входящих props в todolist*/
     title: string,
-    tasks: Array<TaskType> /*сюда передается тип тасок из TaskType.*/
+    tasks: Array<TaskType>, /*сюда передается тип тасок из TaskType.*/
+    removeTasks: Function
 }
 export const TodoList = (props: PropsType) => {
     return (
@@ -19,15 +20,15 @@ export const TodoList = (props: PropsType) => {
             </div>
             <ul>
                 {/*приходят данные из props*/}
-                {props.tasks.map((t)=>{
-                    return(
-                        <li key={t.id}><input type="checkbox" checked={t.isDone}/><span>{t.title}</span>
-                            <button onClick={ ()=> {} }>X</button>
-                        </li>
-
-                    )
-                })}
-
+                {props.tasks.map(t => <li><input type="checkbox" checked={t.isDone}/>
+                        <span>{t.title}</span>
+                        <button onClick={() => {
+                            props.removeTasks(t.id)
+                        }}>X
+                        </button>
+                    </li>
+                )
+                }
             </ul>
             <div>
                 <button>All</button>
