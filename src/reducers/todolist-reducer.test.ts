4d6545f002f0f428540105b123f602b9b3fc9db8
Artem Rubin <catobazzz@gmail.com>
2023-08-01 15:04:34 +0300

    viktor_08_todolistReducer_addTodolist

--- src/reducers/todolist-reducer.test.ts
+++ src/reducers/todolist-reducer.test.ts
@@ -6,15 +6,25 @@ import {todolistReducer} from "./todolist-reducer";
 test('correct todolist should be removed', ()=>{
     const todoListId_1 = v1()
     const todoListId_2 = v1()
-
     const  startState: Array<TodoListType> = [
         {id: todoListId_1, title: "What to learn", filter: "all"},
         {id: todoListId_2, title: "What to buy", filter: "all"},
     ]
-
     const  endState = todolistReducer(startState, {type: "REMOVE-TODOLIST", todolistId: todoListId_1})
-
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todoListId_2)
 
+})
+
+test('correct todolist should be added', ()=>{
+    const todoListId_1 = v1()
+    const todoListId_2 = v1()
+    const newTodolistTitle = "New todolist"
+    const  startState: Array<TodoListType> = [
+        {id: todoListId_1, title: "What to learn", filter: "all"},
+        {id: todoListId_2, title: "What to buy", filter: "all"},
+    ]
+const  endState = todolistReducer(startState, {type: "ADD-TODOLIST", title: newTodolistTitle , todolistId: v1()})
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
 })
--- src/reducers/todolist-reducer.ts
+++ src/reducers/todolist-reducer.ts
@@ -1,13 +1,24 @@
 import {TodoListType} from "../App";
 
+export type TodolistReducerAT = RemoveTodolistAT | AddTodolistAT
+
 export type  RemoveTodolistAT = {
     type: "REMOVE-TODOLIST",
     todolistId: string
 }
-export const todolistReducer = (todolists: Array<TodoListType>, action: RemoveTodolistAT): Array<TodoListType> => {
+export type AddTodolistAT = {
+    type: "ADD-TODOLIST",
+    title: string
+    todolistId: string
+}
+
+export const todolistReducer = (todolists: Array<TodoListType>, action: TodolistReducerAT): Array<TodoListType> => {
     switch (action.type) {
         case "REMOVE-TODOLIST":
             return todolists.filter(tl => tl.id !== action.todolistId)
+        case "ADD-TODOLIST":
+            const newTodo: TodoListType = {id: action.todolistId, title: action.title, filter: "all"}
+            return [...todolists, newTodo]
         default:
             return  todolists
     }
