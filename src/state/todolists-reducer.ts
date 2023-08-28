9492f2a69e8f744aeb6b58e5d29dcaae5262c462
Artem Rubin <catobazzz@gmail.com>
2023-08-28 13:01:02 +0300

    08_addTodolistAC

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,21 +1,36 @@
 import {TodolistType} from "../App";
+import {v1} from "uuid";
 
 export type ActionType = removeTodolistACType
+    | addTodolistACType
 export const todolistsReducer = (state: TodolistType[], action: ActionType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST':
             return state.filter(el => el.id !== action.payload.id)
+        case 'ADD-TODOLIST':
+            const todolist = v1()
+            const newTodolist: TodolistType = {id: todolist, title: action.payload.title, filter: 'all'}
+            return [newTodolist, ...state]
+        // case 'CHANGE-TODOLIST-FILTER':
+        //     return state
+        // case 'UPDATE-TODOLIST-TITLE':
+        //     return state
         default:
             return state
     }
 }
 
 type removeTodolistACType = ReturnType<typeof removeTodolistAC>
+type addTodolistACType = ReturnType<typeof addTodolistAC>
 export const removeTodolistAC = (id: string) => {
     return {
         type: 'REMOVE-TODOLIST',
-        payload: {
-            id
-        }
+        payload: {id}
+    } as const
+}
+export const addTodolistAC = (title: string) => {
+    return {
+        type: 'ADD-TODOLIST',
+        payload: {title}
     } as const
 }
