7a81c9806889e992c6a00313e2ac76e26728cfe4
Artem Rubin <catobazzz@gmail.com>
2023-08-31 16:05:25 +0300

    09_02_removeTaskAC

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -4,19 +4,24 @@ type ActionsType = removeTodolistACType
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
     switch (action.type) {
-        case 'XXX':
-            return state
+        case 'REMOVE-TASK':
+            return {
+                ...state,
+                [action.payload.todolistId]: state[action.payload.todolistId].filter(el => el.id !== action.payload.taskId)
+            }
         default:
             return state
     }
 }
 
-type removeTodolistACType = ReturnType<typeof removeTodolistAC>
+type removeTodolistACType = ReturnType<typeof removeTaskAC>
 
-export const removeTodolistAC = (todolistId: string) => {
+export const removeTaskAC = (taskId: string, todolistId: string) => {
     return {
-        type: 'XXX',
+        type: 'REMOVE-TASK',
         payload: {
+            taskId,
+            todolistId
         }
     }
 }
