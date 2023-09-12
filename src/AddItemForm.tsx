25c6e80aacaeabc7eb71dc70c38a05698d66b3dc
Artem Rubin <catobazzz@gmail.com>
2023-09-12 14:38:39 +0300

    11_02_useCallback

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -8,6 +8,7 @@ type AddItemFormPropsType = {
 }
 
 export const AddItemForm = React.memo((props: AddItemFormPropsType) => {
+    console.log('AddItemForm called')
     let [title, setTitle] = useState("")
     let [error, setError] = useState<string | null>(null)
 
