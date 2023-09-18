737b8fe48470327b94db2270257d95639f450241
Artem Rubin <catobazzz@gmail.com>
2023-09-18 15:31:52 +0300

    12_03_TaskPresentationStory

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,5 +1,5 @@
 import type {Meta, StoryObj} from '@storybook/react';
-import React from "react";
+import React, {useState} from "react";
 import {Task} from "../Task";
 import {action} from "@storybook/addon-actions";
 
@@ -38,3 +38,21 @@ export const TaskIsDoneStory: Story = {
         task: {id: '12wsdewfijdei2343', title: 'CSS', isDone: true},
     },
 };
+
+ const TaskPresentation = () => {
+    const [task, setTask] = useState({id: '12wsdewfijdei2343', title: 'CSS', isDone: false})
+    return <Task
+        changeTaskStatus={()=>{
+        setTask({...task, isDone: !task.isDone})}
+        }
+        changeTaskTitle={(taskId, title)=>{
+        setTask({...task, title: title})}
+        }
+        removeTask={action('removeTask')}
+        task={task}
+        todolistId={'12wsdewfijdei2343'}
+    />
+}
+export const TaskPresentationStory: Story = {
+    render: () => <TaskPresentation />
+}
