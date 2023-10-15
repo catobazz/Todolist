7be6554f6fbb20a233ee378b104131cd26ea6de1
Artem Rybin <catobazz@gmail.com>
2023-10-15 11:17:51 +0300

    02_12 AddItemForm.stories.tsx

---
+++ src/AddItemForm.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {AddItemForm} from "./AddItemForm";
+
+
+export default {
+    title: 'AddItemForm component',
+    component: AddItemForm
+}
+
+export const AddItemFormBaseExample = (props: any) => {
+  return <AddItemForm addItem={(title: string)=>{alert(title)}} />
+}
