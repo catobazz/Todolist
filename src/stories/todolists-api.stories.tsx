d8b4cb2be971e633fbfac3eeac6bb02aa5268e6a
Artem Rubin <catobazzz@gmail.com>
2023-12-20 15:06:30 +0300

    13_03_todolist_api.stories_task_upd_01

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -64,9 +64,10 @@ export const GetTasks = () => {
 export const CreateTask = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        const title = 'REACT'
-        todolistAPI.createTask(title)
-            .then((res) => setState(res.data))
+        const todolistId = '809dba74-2829-4a3a-92f3-bb77b2c72a16'
+        const title = 'What do you learn?'
+        todolistAPI.createTask(todolistId, title)
+            .then((res) => setState(res.data.data.item))
     }, [])
     return <div>{JSON.stringify(state)}</div>
 }
@@ -74,8 +75,9 @@ export const CreateTask = () => {
 export const DeleteTask = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        const todolistId = 'ae125e5f-7934-454e-b475-211ca799bea7'
-        todolistAPI.deleteTask(todolistId)
+        const todolistId = '809dba74-2829-4a3a-92f3-bb77b2c72a16'
+        const taskId = '050bad97-053c-430a-a336-bd5d0961f7d7'
+        todolistAPI.deleteTask(todolistId, taskId)
             .then((res) => setState(res.data))
     }, [])
     return <div>{JSON.stringify(state)}</div>
@@ -84,9 +86,10 @@ export const DeleteTask = () => {
 export const UpdateTaskTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        const title = 'REACTOR'
-        const todolistId = 'd0ab5e2a-1182-41f7-a92e-ced9454c6265'
-        todolistAPI.updateTask(todolistId, title)
+        const title = 'Its updated title'
+        const todolistId = '809dba74-2829-4a3a-92f3-bb77b2c72a16'
+        const taskId = '14b96865-cdd2-4c88-b770-22b4cd9046ac'
+        todolistAPI.updateTask(todolistId, taskId, title)
             .then((res) => setState(res.data))
     }, [])
     return <div>{JSON.stringify(state)}</div>
